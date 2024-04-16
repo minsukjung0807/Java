3f94dddb10587b3898a269979b157f4dcb50a003
minsukjung0807 <164709944+minsukjung0807@users.noreply.github.com>
2024-04-16 23:08:50 +0000

    연산자 Ex28 추가

---
+++ src/main/java/master/chapter3/OperatorEx29.java
@@ -0,0 +1,15 @@
+package master.chapter3;
+
+public class OperatorEx29 {
+    public static void main(String[] args) {
+        byte p = 10;
+        byte n = -10;
+
+        System.out.printf(" p = %d \t%s%n", p, toBinaryString());
+    }
+
+    // 10진 정수를 2진수로 변환하는 메서드
+    static String toBinaryString(int x) {
+        
+    }
+}
