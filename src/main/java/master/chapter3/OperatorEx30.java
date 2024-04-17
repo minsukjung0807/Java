b50576d9719cdad4fbf887401b5c1b5bd922ae83
minsukjung0807 <164709944+minsukjung0807@users.noreply.github.com>
2024-04-17 02:59:29 +0000

    연산자 Ex32추가

--- src/main/java/master/chapter3/OperatorEx30.java
+++ src/main/java/master/chapter3/OperatorEx30.java
@@ -9,8 +9,30 @@ public class OperatorEx30 {
         return tmp.substring(tmp.length() - 32);
     }
     public static void main(String[] args) {
-        int dec = 8;
+        int dec = 10;
 
-        System.
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 0, dec >> 0, toBinaryString(dec >> 0));
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 1, dec >> 1, toBinaryString(dec >> 1));
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 2, dec >> 2, toBinaryString(dec >> 2));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 0, dec << 0, toBinaryString(dec << 0));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 1, dec << 1, toBinaryString(dec << 1));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 2, dec << 2, toBinaryString(dec << 2));
+        System.out.println();
+
+        dec = -10;
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 0, dec >> 0, toBinaryString(dec >> 0));
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 1, dec >> 1, toBinaryString(dec >> 1));
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 2, dec >> 2, toBinaryString(dec >> 2));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 0, dec << 0, toBinaryString(dec << 0));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 1, dec << 1, toBinaryString(dec << 1));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 2, dec << 2, toBinaryString(dec << 2));
+
+        dec = 8;
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 0, dec >> 0, toBinaryString(dec >> 0));
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 32, dec >> 32, toBinaryString(dec >> 32));
+
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 31, dec >> 31, toBinaryString(dec >> 31));
+
+
     }
 }
