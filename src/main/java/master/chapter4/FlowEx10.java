c9cceee1b484c1e5e49e86e5b204658ed5071e90
minsukjung0807 <164709944+minsukjung0807@users.noreply.github.com>
2024-04-17 13:50:59 +0000

    조건문과 반복문 Ex15추가

---
+++ src/main/java/master/chapter4/FlowEx10.java
@@ -0,0 +1,30 @@
+package master.chapter4;
+import java.util.*;
+
+public class FlowEx10 {
+    public static void main(String[] args) {
+        char grade = ' ';
+
+        System.out.print("당신의 점수를 입력하세요. (1~100)>");
+
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+
+        switch(score / 10) {
+            case 10:
+            case 9:
+                grade = 'A';
+                break;
+            case 8:
+                grade = 'B';
+                break;
+            case 7:
+                grade = 'C';
+                break;
+            default:
+                grade ='F';
+        }
+
+        System.out.println("당신의 학점은 " + grade + "입니다.");
+    }
+}
