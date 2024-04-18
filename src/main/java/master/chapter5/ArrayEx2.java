9df20b27d702709fda994788212a4f451e1efc6f
minsukjung0807 <164709944+minsukjung0807@users.noreply.github.com>
2024-04-18 13:31:49 +0000

    배열 Ex2 추가

---
+++ src/main/java/master/chapter5/ArrayEx2.java
@@ -0,0 +1,12 @@
+package master.chapter5;
+import java.util.*;
+
+public class ArrayEx2 {
+    public static void main(String[] args) {
+        int[] iArr1 = new int[10];
+        int[] iArr2 = new int[10];
+        // int[] iArr3 = new int[] {100, 95, 80, 70, 60};
+        int[] iArr3 = {100, 95, 80, 70, 60};
+        char[] chArr = {'a', 'b', 'c', 'd'};
+    }
+}
