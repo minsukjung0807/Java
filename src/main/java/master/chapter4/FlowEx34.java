51c22141b0c9b58ce3099df21e122908516b148d
minsukjung0807 <164709944+minsukjung0807@users.noreply.github.com>
2024-04-18 11:36:51 +0000

    조건문과 반복문 Ex34 추가

---
+++ src/main/java/master/chapter4/FlowEx34.java
@@ -0,0 +1,10 @@
+package master.chapter4;
+import java.util.*;
+
+public class FlowEx34 {
+    public static void main(String[] args) {
+        int menu = 0, num = 0;
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}
