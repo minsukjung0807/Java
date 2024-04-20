194a7a45256b5cc3f21fbf874f71a1c112acb9e3
minsukjung0807 <164709944+minsukjung0807@users.noreply.github.com>
2024-04-20 08:59:11 +0000

    배열 Ex10추가

---
+++ src/main/java/master/chapter5/ArrayEx10.java
@@ -0,0 +1,18 @@
+package master.chapter5;
+
+public class ArrayEx10 {
+    public static void main(String[] args) {
+        int[] numArr = new int[10];
+
+        for(int i = 0; i < numArr.length; i++) {
+            System.out.print(numArr[i] = (int) (Math.random() * 10));
+        }
+        System.out.println();
+
+        for(int i = 0; i < numArr.length - 1; i++) {
+            boolean changed = false;    // 자리 바꿈이 발생했는지를 체크한다.
+
+            for(int j = 0 ;)
+        }
+    }   
+}
